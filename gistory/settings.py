49f9a8db4162af791f9ccf97e7092e31faec5869
adriantre <adrianto@stud.ntnu.no>
2016-12-08 15:02:43 +0100

    chabged template dir in settings.py

--- gistory/settings.py
+++ gistory/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'gistory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
